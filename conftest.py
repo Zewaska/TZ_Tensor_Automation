53e628ea6d0e4529a041d3f636ca9da80c5697bd
Zewaska <pikalovyaros@gmail.com>
2022-09-06 23:24:24 +0300

    добавлена опция игнорирования error:handshake failed

--- conftest.py
+++ conftest.py
@@ -1,9 +1,12 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope="session")
 def browser(request):
-    browser = webdriver.Chrome()
+    options = Options()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
