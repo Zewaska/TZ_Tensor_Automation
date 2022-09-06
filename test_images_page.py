17809a91d671a055553df1f009fc7af7aad44db3
Zewaska <pikalovyaros@gmail.com>
2022-09-06 23:28:11 +0300

    выполнены шаги с 8 по 12

--- test_images_page.py
+++ test_images_page.py
@@ -1,15 +1,18 @@
 from .pages.base_page import BasePage
 from .pages.images_page import ImagesPage
-import time
 
 
 def test_guest_should_be_see_yandex_images(browser):
     page = BasePage(browser)
     page.open()
-    page.should_be_images_page()
+    page.should_be_link_images()
     page.go_to_images_page()
     image_page = ImagesPage(browser, url = browser.current_url)
     image_page.open_first_popular_category()
     image_page.should_be_see_name_category_in_search_box()
-
-    time.sleep(5)
+    image_page.open_first_small_image()
+    image_page.should_be_open_first_image()
+    image_page.press_button_next()
+    image_page.should_be_open_second_image()
+    image_page.press_button_forward()
+    image_page.should_be_image_from_step_eaght()
