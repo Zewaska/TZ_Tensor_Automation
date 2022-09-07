88d6b523e7e8eae1712062b69f8bdcfb4df63654
Zewaska <pikalovyaros@gmail.com>
2022-09-07 14:53:55 +0300

    Description of steps in test-cases

--- test_images_page.py
+++ test_images_page.py
@@ -6,18 +6,40 @@ from .pages.main_page import MainPage
 
 
 def test_guest_should_be_see_yandex_images(browser):
+    # Создаем экземпляр класса, заходим на yandex.ru
     page = MainPage(browser)
     page.open()
+
+    # Проверяем, что ссылка Картинки присутствует
     page.should_be_link_images()
+
+    # Кликаем на ссылку Картинки, создаем экземпляр класса ImagesPage
     page.go_to_images_page()
     image_page = ImagesPage(browser, url = browser.current_url)
+
+    # Проверяем, что перешли на https://yandex.ru/images/
     image_page.should_be_images_in_url()
+
+    # Открываем первую популярную категорию картинок
     image_page.open_first_popular_category()
+
+    # Проверяем, что название категории отображается в поле поиска
     image_page.should_be_see_name_category_in_search_box()
+
+    # Открываем первую картинку
     image_page.open_first_small_image()
+
+    # Проверяем что первая картинка открылась
     image_page.should_be_open_first_image()
+
+    # Нажимаем кнопку вперед
     image_page.press_button_next()
+
+    # Проверяем, что картинка сменилась
     image_page.should_be_open_second_image()
+
+    # Нажимаем кнопку назад
     image_page.press_button_forward()
+
+    # Проверяем, что открылась первая картинка
     image_page.should_be_image_from_step_eaght()
- 
--- test_search_page.py
+++ test_search_page.py
@@ -6,12 +6,24 @@ from .pages.search_page import SearchPage
 
 
 def test_guest_should_be_search_in_yandex(browser):
+    # Создаем экземпляр класса, заходим на yandex.ru
     page = MainPage(browser)
     page.open()
-    page.should_be_search_box()
-    page.input_search_tensor()
-    page.should_be_see_suggest()
-    search_page = SearchPage(browser)
-    search_page.should_be_see_result_search_after_press_enter()
-    search_page.should_be_first_link_tensor()
 
+    # Проверяем наличие поля поиска
+    page.should_be_search_box()
+
+    # Вводим в поле поиска Тензор
+    page.input_search_tensor()
+
+    # Проверяем появление таблицы с подсказками
+    page.should_be_see_suggest()
+
+    # Создаем экземпляр класса SearchPage
+    search_page = SearchPage(browser)
+
+    # Проверяем появление таблицы результатов после нажатия Enter
+    search_page.should_be_see_result_search_after_press_enter()
+
+    # Проверяем что первая ссылка ведет на tensor.ru
+    search_page.should_be_first_link_tensor()
